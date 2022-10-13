5be4dd6015c6c055f45994893fcca511b94e4085
joshdoyle94 <joshdoyle94@gmail.com>
2022-10-12 21:20:41 -0400

    added first version of backend infra

---
+++ models/workouts.js
@@ -0,0 +1,31 @@
+// import dependencies
+const mongoose = require('./connection')
+
+// import user model for populate
+const User = require('./user')
+
+const feedbackSchema = require('./feedback')
+
+// destructure the schema and model constructors from mongoose
+const { Schema, model } = mongoose
+
+const workoutsSchema = new Schema({
+		name: { type: String, required: true },
+		description: { type: String, required: true },
+        difficulty: { type: Number, required: true },
+		isVis: { type: Boolean, required: true },
+		author: {
+			type: Schema.Types.ObjectID,
+			ref: 'User',
+		},
+        feedback: [feedbackSchema]
+	},
+	{ timestamps: true }
+)
+
+const Workouts = model('Workouts', workoutsSchema)
+
+/////////////////////////////////
+// Export our Model
+/////////////////////////////////
+module.exports = Workouts
